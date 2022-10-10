71b7234d1199a02c58b0f6cd68fa1f23ad49a624
jaewooMaeng <maeng9584@likelion.org>
2022-10-11 03:13:12 +0900

    task2

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Hero(models.Model):
     name = models.CharField(max_length=120)
+    age = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.hero_list),
     path('<int:id>/', views.id, name='hero_id'),
     path('<str:name>/', views.name, name='hero_name'),
+    path('info/<int:id>/', views.hero_info, name='hero_info'),
 ]
