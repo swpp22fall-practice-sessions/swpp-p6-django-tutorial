b98c0afbc3fc048ab0d7709e65b9b8cdcf3e5653
Yujin Im <jina0605@gmail.com>
2022-10-12 19:29:29 +0900

    Implement /hero/:id and /hero/:name

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>', views.hero_id, name='hero_id'),
+    path('<str:name>', views.hero_name, name='hero_name'),
 ]
--- toh/hero/views.py
+++ toh/hero/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse('Hello, World!')
+
+def hero_id(request, id=0):
+    return HttpResponse(f'Your id is {id}!')
+
+def hero_name(request, name=""):
+    return HttpResponse(f'Your name is {name}!')
