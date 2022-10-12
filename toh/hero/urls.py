9f7442b193d58957ab02680573fcfb7d26cabecd
Yujin Im <jina0605@gmail.com>
2022-10-12 19:52:20 +0900

    Implement hero_list

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:id>', views.hero_id, name='hero_id'),
-    path('<str:name>', views.hero_name, name='hero_name'),
+    path('', views.hero_list)
 ]
--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,11 +1,24 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from json.decoder import JSONDecodeError
 
-def index(request):
-    return HttpResponse('Hello, World!')
+from hero.models import Hero
 
-def hero_id(request, id=0):
-    return HttpResponse(f'Your id is {id}!')
-
-def hero_name(request, name=""):
-    return HttpResponse(f'Your name is {name}!')
+@csrf_exempt
+def hero_list(request):
+    if request.method == 'GET':
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == 'POST':
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)['name']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {'id': hero.id, 'name': hero.name}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
