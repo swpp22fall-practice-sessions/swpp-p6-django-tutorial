a9b12713db70c9e6eee53ad8bedd3c42a07f2743
askrid <joonwoo3023@gmail.com>
2022-10-12 20:42:16 +0900

    finish practice session

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.hero_list, name="hero_list"),
+    path("info/<int:id>/", views.hero_info, name="hero_info"),
     path("<int:id>/", views.echo_id, name="echo_id"),
     path("<str:name>/", views.echo_name, name="echo_name"),
 ]
--- toh/hero/views.py
+++ toh/hero/views.py
@@ -1,13 +1,57 @@
-from django.http import HttpRequest, HttpResponse
+import json
+
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_GET, require_http_methods
+
+from hero.models import Hero
 
 
-def index(request: HttpRequest):
-    return HttpResponse("Hello, world!")
+@csrf_exempt
+@require_http_methods(["GET", "POST"])
+def hero_list(request: HttpRequest):
+    if request.method == "GET":
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        return JsonResponse(hero_all_list, safe=False)
+    elif request.method == "POST":
+        try:
+            body = request.body.decode()
+            body_dict = json.loads(body)
+            hero = Hero.objects.create(**body_dict)
+        except (json.JSONDecodeError, TypeError):
+            return HttpResponseBadRequest
+        data = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(data, status=201)
 
 
+@csrf_exempt
+@require_http_methods(["GET", "PUT"])
+def hero_info(request: HttpRequest, id: int):
+    try:
+        hero = Hero.objects.get(id=id)
+    except Hero.DoesNotExist:
+        return HttpResponseBadRequest
+
+    if request.method == "GET":
+        data = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(data)
+    elif request.method == "PUT":
+        body = request.body.decode()
+        body_dict = json.loads(body)
+        for attr, value in body_dict.items():
+            if attr not in {"name", "age"}:
+                return HttpResponseBadRequest
+            setattr(hero, attr, value)
+        hero.save()
+        data = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(data)
+
+
+@require_GET
 def echo_id(request: HttpRequest, id: int):
     return HttpResponse(f"Your id is {id}!")
 
 
-def echo_name(requset: HttpRequest, name: str):
+@require_GET
+def echo_name(request: HttpRequest, name: str):
     return HttpResponse(f"Your name is {name}!")
