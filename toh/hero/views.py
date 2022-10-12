7491c433a2bb2f5a88edd932c0aee8ba0f090c58
notiona <notiona@snu.ac.kr>
2022-10-12 19:33:46 +0900

    task 1

--- toh/hero/views.py
+++ toh/hero/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World!')
+
+def hero_id(request, id=""):
+    return HttpResponse(f"Your id is {id}!")
+
+def hero_name(request, name=""):
+    return HttpResponse(f"Your name is {name}!")
