fe42490b9d8ab6da236ca9b9636b5c5182937622
Yujin Im <jina0605@gmail.com>
2022-10-12 20:23:15 +0900

    Make Team model

--- toh/hero/models.py
+++ toh/hero/models.py
@@ -6,3 +6,18 @@ class Hero(models.Model):
 
     def __str__(self):
         return self.name
+
+class Team(models.Model):
+    name = models.CharField(max_length=120)
+    leader = models.ForeignKey(
+        Hero,
+        on_delete=models.CASCADE,
+        related_name='leader_set',
+    )
+    members = models.ManyToManyField(
+        Hero,
+        related_name='teams',
+    )
+
+    def __str__(self):
+        return self.name
