b59539ba31ffa17e07459b5c1cffc1cb2130cd77
ice1039 <jl6880@daum.net>
2022-10-12 20:53:01 +0900

    Task 3 done

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name = 'index'),
+    path('', views.hero_list),
     path('<int:id>/', views.captureId, name='captureId'),
     path('<str:name>/', views.captureName, name='captureName'),
-    
+    path('info/<int:id>/', views.hero_info, name='hero_info'),
 ]
