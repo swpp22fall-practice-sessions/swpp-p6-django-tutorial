fa143892935a6d4afc0e71592aa071cb239dfbb6
Pabiya <junsu0115@snu.ac.kr>
2022-10-12 19:14:48 +0900

    2

--- toh/hero/urls.py
+++ toh/hero/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.hero_list),
+    # path('', views.index, name='index'),
+    path('<int:id>/', views.id, name='hero_id'),
+    path('<str:name>/', views.name, name='hero_name'),
 ]
