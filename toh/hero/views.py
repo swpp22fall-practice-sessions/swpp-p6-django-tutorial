7c6472b7a0f8f9b4a8f4783c4ab461b2f17cdaa4
Junyoung Yeom <yeomjy111@gmail.com>
2022-10-13 19:32:48 +0900

    task 123

---
+++ toh/hero/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import (
+    HttpResponseBadRequest,
+    HttpResponseNotAllowed,
+    JsonResponse,
+    HttpResponse,
+)
+from django.views.decorators.csrf import csrf_exempt
+import json
+from json.decoder import JSONDecodeError
+from .models import Hero
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+
+def id(request, id=0):
+    return HttpResponse(f"Your id is {id}!")
+
+
+def name(request, name=""):
+    return HttpResponse(f"Your name is {name}!")
+
+
+@csrf_exempt
+def hero_list(request):
+    if request.method == "GET":
+        hero_all_list = [hero for hero in Hero.objects.all().values()]
+        # for hero in hero_all_list:
+        #     hero["age"] = str(hero["age"])
+        return JsonResponse(hero_all_list, safe=False)
+
+    elif request.method == "POST":
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)["name"]
+
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+
+        hero = Hero(name=hero_name)
+        hero.save()
+        response_dict = {"id": hero.id, "name": hero.name}
+        return JsonResponse(response_dict, status=201)
+
+    else:
+        return HttpResponseNotAllowed(["GET", "POST"])
+
+
+@csrf_exempt
+def hero_info(request, id):
+    if request.method == "GET":
+        hero = Hero.objects.get(id=id)
+        return JsonResponse({"id": hero.id, "name": hero.name, "age": hero.age})
+    elif request.method == "PUT":
+        try:
+            body = request.body.decode()
+            hero_name = json.loads(body)["name"]
+            hero_age = json.loads(body)["age"]
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        hero = Hero.objects.get(id=id)
+        hero.name = hero_name
+        hero.age = hero_age
+        hero.save()
+        response_dict = {"id": hero.id, "name": hero.name, "age": hero.age}
+        return JsonResponse(response_dict, status=200)
+    else:
+        return HttpResponseNotAllowed(["GET", "PUT"])
