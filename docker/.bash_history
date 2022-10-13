dd498d032f0b46c8a4f35fad8391eb7ca9871a73
keetae423 <55076957+keetae423@users.noreply.github.com>
2022-10-13 10:02:10 +0900

    lab06

---
+++ docker/.bash_history
@@ -0,0 +1,5 @@
+python manage.py migrate
+django
+pip install django
+python manage.py migrate
+python manage.py startapp hero
---
+++ toh/hero/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('<str:name>/', views.hero_name, name='hero_name'),
+    path('<int:id>/', views.hero_id, name='hero_id'),
+    path('info/<int:id>/', views.hero_info, name='hero_info'),
+    path('', views.hero_list)
+]
